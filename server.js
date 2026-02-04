fb3ea6af526a798a82dbf47012c5d1abfc1c2563
Simone Ghosh <simone.ghoshbd@gmail.com>
2026-02-04 15:01:27 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+
+// Shema 
+const typeDefs =` 
+    type Query {
+        hello: String
+    }
+`;
+
+//Resolvers
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello, GraphQL',
+    },
+};
+
+//Start server 
+const server = new ApolloServer({ typeDefs, resolvers });
+
+startStandaloneServer(server, {
+    listen: { port:4000 },
+}).then(() => {
+    console.log('Server ready at <http://localhost:4000>')
+})
+
+
+
